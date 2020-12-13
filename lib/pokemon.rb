d5cd7b082cf19af0b26bf06c3af88ea1c1c47c71
Jose Melero Martinez <jmelero08@gmail.com>
2020-12-13 00:54:16 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,8 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :hp, :db
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
 end
